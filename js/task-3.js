773137d03a26c4aacd321c66d5457103aaa3ee14
Oleksi-Dobrovolskyi <alex.puhovski@gmail.com>
2024-11-17 14:53:56 +0100

    Add files via upload

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,20 @@
-function checkForSpam(message) {
- 
-  const lowerCaseMessage = message.toLowerCase();
+function filterArray(numbers, value) {
+  const result = []; 
 
- 
-  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+  
+  for (let a = 0; a < numbers.length; a++) {
+    if (numbers[a] > value) {
+      result.push(numbers[a]);
+    }
+  }
 
+  return result;
 }
 
-console.log(checkForSpam("Latest technology news")); 
-console.log(checkForSpam("JavaScript weekly newsletter")); 
-console.log(checkForSpam("Get best sale offers now!")); 
-console.log(checkForSpam("Amazing SalE, only tonight!")); 
-console.log(checkForSpam("Trust me, this is not a spam message")); 
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
-console.log(checkForSpam("[SPAM] How to earn fast money?")); 
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38));
+console.log(filterArray([12, 24, 8, 41, 76], 20));
+ 
